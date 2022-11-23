62df1e6fe40b121ff10227304bc9776f2cbc7287
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2022-11-23 18:49:31 +0700

    Fitur Finished
     Sudah connect ke database

--- object/Stan/stan.js
+++ object/Stan/stan.js
@@ -1,29 +1,17 @@
 const db = require("../../database")
 class Stan {
-    constructor(id, name, path_photo, menu) {
-        this.id = id
-        this.name = name
-        this.path_photo = path_photo
-        this.photo_menu = photo_menu
+    constructor(id_stan, nama_stan, path_logo, path_menu) {
+        this.id_stan = id_stan
+        this.nama_stan = nama_stan
+        this.path_logo = path_logo
+        this.path_menu = path_menu
     }
-    static async getDataWithId(id){
-        let data = await db.pool.query("select * from stan where id=${id}")
+    static async getDataWithId(id_stan){
+        let data = await db.pool.query("select * from stan where id_stan=${id_stan}")
         return data
     }
     static async getData(){
         let data = await db.pool.query("select * from stan")
-        return data
-    }
-    static async getName(id){
-        let data = await db.pool.query("select name from stan where id=${id}")
-        return data
-    }
-    static async getPhoto(id){
-        let data = await db.pool.query("select path_photo from stan where id=${id}")
-        return data
-    }
-    static async getMenu(id){
-        let data = await db.pool.query("select photo_menu from stan where id=${id}")
         return data
     }
 }
