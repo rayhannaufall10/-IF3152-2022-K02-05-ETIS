4c341ff3cbe8e75864110cc2c643016ce176ede1
akMALE21 <18220040@std.stei.itb.ac.id>
2022-11-23 18:35:15 +0700

    FIX HTML CSS DAN JS FITUR UPGRADE

--- FE/Page/Ticket/UpTicket/index.js
+++ FE/Page/Ticket/UpTicket/index.js
@@ -1,68 +1,53 @@
 const ticketModule = require("../../../../object/Tiket/tiket");
 const moment = require("moment");
 const form = document.getElementById("form");
-const continueButton = document.getElementById("continueButton");
-// Set Variable for Modal element
-let modalBG = document.getElementsByClassName("modal-background");
-modalBG = modalBG[0];
 
-
+// Mengambil Id Ticket dari input dan melakukan pengecekan pada database
 async function mengambil_id(e){
   e.preventDefault()
   var id_aktual = document.getElementById('id_ticket').value
   var id_data = await ticketModule.Ticket.getDataWithId(id_aktual)
   
+  // Id ticket yang diinput tidak terdapat pada database
   if (!id_data.length){
     var modal = document.getElementById("modalGaada");
     var span = document.getElementsByClassName("closeGaada")[0];
     modal.style.display = "flex";
-    //form.reset();
+    form.reset();
   }
-  else{
+  else{ 
     var fp_data = id_data[0].fast_pass
+    // Id ticket belum fastpass
     if (fp_data == 0){
       ticketModule.Ticket.setUpTicket(id_aktual)
       var modal = document.getElementById("modalBelum");
       var span = document.getElementsByClassName("closeBelum")[0];
       modal.style.display = "flex";
+      form.reset();
     }
-    else{
-      console.log("tiket sudah fast pass")
+    else{ // Id ticket sudah fastpass
       var modal = document.getElementById("modalSudah");
       var span = document.getElementsByClassName("closeSudah")[0];
       modal.style.display = "flex";
+      form.reset();
     }
   }
-  // When the user clicks on <span> (x), close the modal
+  //close the modal, dengan klik X
   span.onclick = function() {
     modal.style.display = "none";
   }
-  // When the user clicks anywhere outside of the modal, close it
+  // close the modal, dengan klik diluar modal
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
 }
-      
-function showDoneConfirm(){
-  let modalDone = document.getElementsByClassName('modal-done')[0]
-  const done = document.getElementsByClassName('done')[0]
-  const loading =document.getElementsByClassName('loading')[0]
-  modalDone.style.display="flex"
-  setTimeout(() => {
-    loading.style.display = "none"
-    done.style.display = 'flex'
-    // goBack()
-    setTimeout(()=>{
-      history.go(-2)
-    },900)
-    
-  }, 2000);  
-}
 
+// Klik pada clear button
 document.getElementById("clearButton").addEventListener("click", () => {
   form.reset();
 });
 
+// Klik pada continue button
 document.getElementById("continueButton").addEventListener("click", mengambil_id, false);
