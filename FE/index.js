fbe9144343a2a23d128aa64e1cf6142d1a5debfb
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2022-11-23 18:20:51 +0700

    fix pesan tiket saat data kosong

--- FE/index.js
+++ FE/index.js
@@ -1,3 +1,8 @@
 const electron = require('electron')
 const ticketModule = require("../object/Tiket/tiket")
 const ipc = electron.ipcRenderer
+const btn = document.getElementById('mybtn')
+btn.addEventListener('click', async ()=>{
+    await ticketModule.Ticket.setLastIdTicket()
+    
+})
